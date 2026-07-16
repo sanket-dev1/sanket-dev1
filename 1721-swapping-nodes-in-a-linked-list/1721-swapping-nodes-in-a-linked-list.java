483de417a76490fab576202aca79bcfe75f79160
Sanket Sawant <sawantsanket364@gmail.com>
2026-07-16 15:58:39 +0530

    Time: 1 ms (100%), Space: 118.5 MB (86.04%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -8,29 +8,61 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode swapNodes(ListNode head, int k) {
+//         if(head==null || head.next==null){
+//             return head;
+//         }
+//         ListNode first=head;
+//         for(int i=1;i<k;i++){
+//             first=first.next;
+//         }
+//         int count=0;
+//         ListNode temp=head;
+//         while(temp != null){
+//             count++;
+//             temp=temp.next;
+//         }
+//         int skip=count-k;
+//         ListNode second=head;
+//         for(int i=0;i<skip;i++){
+//             second=second.next;
+//         }
+//         int tempk=first.val;
+//         first.val=second.val;
+//         second.val=tempk;
+//         return head;
+//     }
+// } 
 class Solution {
-    public ListNode swapNodes(ListNode head, int k) {
-        if(head==null || head.next==null){
-            return head;
+    public ListNode swapNodes(ListNode head, int k) {		
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode first = head, second = head;
+        
+		// Put fast (k-1) nodes after slow
+        for(int i = 0; i < k - 1; ++i)
+            fast = fast.next;
+            
+		// Save the node for swapping
+        first = fast;
+
+		// Move until the end of the list
+        while(fast.next != null) {
+			slow = slow.next;
+            fast = fast.next;
         }
-        ListNode first=head;
-        for(int i=1;i<k;i++){
-            first=first.next;
-        }
-        int count=0;
-        ListNode temp=head;
-        while(temp != null){
-            count++;
-            temp=temp.next;
-        }
-        int skip=count-k;
-        ListNode second=head;
-        for(int i=0;i<skip;i++){
-            second=second.next;
-        }
-        int tempk=first.val;
-        first.val=second.val;
-        second.val=tempk;
+        
+        // Save the second node for swapping
+		// Note that the pointer second isn't necessary: we could use slow for swapping as well
+		// However, having second improves readability
+        second = slow;
+		
+		// Swap values
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+        
         return head;
     }
-} 
+}
