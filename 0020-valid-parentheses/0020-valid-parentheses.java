8b3eda370f53b92f266657fe521e09470b2e2fe4
Sanket Sawant <sawantsanket364@gmail.com>
2026-07-27 17:54:24 +0530

    Time: 3 ms (86.28%), Space: 43.2 MB (58.17%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,27 +1,53 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> stack=new Stack<>();
+
+//         for(Character ch:s.toCharArray()){
+//             if(ch=='(' || ch=='{' || ch=='['){
+//             stack.push(ch);
+//             }else{
+//             if(ch==')'){
+//                 if(stack.isEmpty() || stack.pop() != '('){
+//                     return false;
+//                 }
+//             }
+//             if(ch=='}'){
+//                 if(stack.isEmpty() || stack.pop() != '{'){
+//                     return false;
+//                 }
+//             }
+//             if(ch==']'){
+//                 if(stack.isEmpty() || stack.pop() != '['){
+//                     return false;
+//                 }
+//             }
+//         }
+//         }
+//         return stack.isEmpty();
+//     }
+// }
+
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack=new Stack<>();
-
-        for(Character ch:s.toCharArray()){
-            if(ch=='(' || ch=='{' || ch=='['){
-            stack.push(ch);
-            }else{
-            if(ch==')'){
-                if(stack.isEmpty() || stack.pop() != '('){
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+                if (ch == '}' && top != '{') {
                     return false;
                 }
             }
-            if(ch=='}'){
-                if(stack.isEmpty() || stack.pop() != '{'){
-                    return false;
-                }
-            }
-            if(ch==']'){
-                if(stack.isEmpty() || stack.pop() != '['){
-                    return false;
-                }
-            }
-        }
         }
         return stack.isEmpty();
     }
