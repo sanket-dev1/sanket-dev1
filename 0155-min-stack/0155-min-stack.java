d5856275499bba78d7e535adaa6e12c874ed98bb
Sanket Sawant <sawantsanket364@gmail.com>
2026-07-28 18:15:42 +0530

    Time: 37 ms (13.64%), Space: 102.2 MB (24.67%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,44 +1,65 @@
-// class MinStack {
-//     Stack<Integer> st;
+import java.util.Stack;
 
-//     public MinStack() {
-//         st=new Stack<>();
-//     }
-    
-//     public void push(int value) {
-//         st.push(value);
-//     }
-    
-//     public void pop() {
-//         if(!st.isEmpty()){
-//             st.pop();
-//         }
-//     }
-    
-//     public int top() {
-//         if(!st.isEmpty()){
-//             st.peek();
-//         }
-//     }
-    
-//     public int getMin() {
-        
-//     }
-// }
+class Pair {
+    int first;
+    int second;
+
+    Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
 
 class MinStack {
-    Stack<Integer> s = new Stack<>(), st = new Stack<>();
-    public void push(int val) {
-        s.push(val);
-        if (st.isEmpty() || val <= st.peek()) st.push(val);
+    Stack<Pair> st;
+
+    public MinStack() {
+        st = new Stack<>();
     }
+
+    public void push(int value) {
+        if (st.isEmpty()) {
+            st.push(new Pair(value, value));
+        } else {
+            int minVal = Math.min(value, st.peek().second);
+            st.push(new Pair(value, minVal));
+        }
+    }
+
     public void pop() {
-        if (s.peek().equals(st.peek())) st.pop();
-        s.pop();
+        if (!st.isEmpty()) {
+            st.pop();
+        }
     }
-    public int top() { return s.isEmpty() ? -1 : s.peek(); }
-    public int getMin() { return st.isEmpty() ? -1 : st.peek(); }
+
+    public int top() {
+        if (!st.isEmpty()) {
+            return st.peek().first;
+        }
+        return -1; // or throw an exception
+    }
+
+    public int getMin() {
+        if (!st.isEmpty()) {
+            return st.peek().second;
+        }
+        return -1; // or throw an exception
+    }
 }
+
+// class MinStack {
+//     Stack<Integer> s = new Stack<>(), st = new Stack<>();
+//     public void push(int val) {
+//         s.push(val);
+//         if (st.isEmpty() || val <= st.peek()) st.push(val);
+//     }
+//     public void pop() {
+//         if (s.peek().equals(st.peek())) st.pop();
+//         s.pop();
+//     }
+//     public int top() { return s.isEmpty() ? -1 : s.peek(); }
+//     public int getMin() { return st.isEmpty() ? -1 : st.peek(); }
+// }
 
 
 /**
