f313ccc6498778fcaacfb880aa8bb42fff408e66
Sanket Sawant <sawantsanket364@gmail.com>
2026-07-31 18:58:41 +0530

    Time: 10 ms (66.76%), Space: 48.1 MB (52.6%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int arr[]=new int[26];
+        for(char ch:word.toCharArray()){
+            arr[ch-'a']++;
+        }
+        Arrays.sort(arr);
+        int count=0;
+        int minkey=0;
+        for(int i=25;i>=0;i--){
+            int val=arr[i]*(count/8+1);
+            minkey+=val;
+            count++;
+        }
+        return minkey;
+    }
+}
